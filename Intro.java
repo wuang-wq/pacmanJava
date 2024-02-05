322893fe6df5c704961043bc72ed7bacc7a8c961
PhuongNam277 <namp3411@gmail.com>
2024-02-05 21:14:22 +0700

    commit 2

--- Intro.java
+++ Intro.java
@@ -1,47 +1,41 @@
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.BorderLayout;
 import java.awt.Font;
-import java.awt.GridLayout;
 
 
-public class Intro extends JFrame{
+public class Intro{
     JPanel introPanel;
     JButton Start;
     JButton Exit;
     Intro(){
         //set up panel
-        this.setLayout(new BorderLayout());
-        introPanel = new JPanel();
+        introPanel = new JPanel(null);
         introPanel.setSize(615, 615);
-        // introPanel.setOpaque(true);
-        introPanel.setLayout(new GridLayout(2, 1));
+        introPanel.setOpaque(true);
         //Code phần giao diện
         this.init();
     }
 
     public void init() {
         // Set font (Có thể set nhiều font với kích thước khác nhau, chỉ cần đặt khác tên đi là được)
-        Font font = new Font("Arial", Font.BOLD, 32);
+        Font font = new Font("Arial", Font.BOLD, 16);
 
 
         // Set các thành phần cơ bản cho ứng dụng
-        this.setSize(615, 615);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
+        introPanel.setVisible(true);
 
         // Tạo ra một nút Start và đặt nội dung cho nó là Start
         Start = new JButton("Start");
         Start.setFont(font);
+        Start.setBounds(50, 300, 100, 50);
+
+        // Làm cho nút trở nên trong suốt
+        // Start.setOpaque(false);
+        // Start.setContentAreaFilled(false);
+        // Start.setBorderPainted(false);
 
         // Thêm các thành phần vào giao diện chính
         introPanel.add(Start);
-
-        
-        // Add thành phần introPanel vào giao diện chính của intro
-        this.add(introPanel, BorderLayout.CENTER);
 
     }
 }
