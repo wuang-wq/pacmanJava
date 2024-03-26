5e606f2d58623ea9505e2c80ef2d647852e341d8
wuang-wq <128731869+wuang-wq@users.noreply.github.com>
2024-03-26 16:27:18 +0700

    Add files via upload
    update

--- State/Intro.java
+++ State/Intro.java
@@ -2,6 +2,9 @@ package State;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+
+import main.MyFrame;
+
 import java.awt.Font;
 import java.awt.Color;
 import javax.swing.ImageIcon;
@@ -10,15 +13,20 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 
-
+ 
 public class Intro{
-   public JPanel introPanel;
-    public JButton Start;
-    public JButton Exit;
-    public Intro(){
+    MyFrame Mf;
+    private JPanel introPanel;
+    private JButton Start;
+    private JButton Exit;
+    private JButton loginButton;
+    private JButton scoreBoard;
+
+    public Intro(MyFrame Mf){
+        this.Mf=Mf;
         //set up panels
         introPanel = new JPanel(null);
-        introPanel.setSize(615, 615);
+        introPanel.setSize(Mf.getJframeWidth(),Mf.getJframeHeightParent());
         introPanel.setOpaque(true);
         //Code phần giao diện
         this.init();
@@ -26,53 +34,78 @@ public class Intro{
 
     public void init() {
         // Set font (Có thể set nhiều font với kích thước khác nhau, chỉ cần đặt khác tên đi là được)
-        Font font = new Font("Arial", Font.BOLD, 24);
-
+        Font font = new Font("Arial", Font.BOLD, 28);
 
         // Set các thành phần cơ bản cho ứng dụng
         introPanel.setVisible(true);
         
         // Tạo ra một JLabel để chứa hình ảnh làm nền
-        JLabel background = new JLabel(new ImageIcon("./picture/bgr.png"));
-        background.setBounds(0, 0, 615, 615);
+        JLabel background = new JLabel(new ImageIcon("./picture/Background_Intro.png"));
+        background.setBounds(0, 0, Mf.getJframeWidth(), Mf.getJframeHeightParent());
 
         // Thêm hình ảnh làm nền vào introPanel
         introPanel.add(background);
 
-        // Tạo ra một nút Start và đặt nội dung cho nó là Start
-        Start = new JButton("Start");
-        Start.setFont(font);
-        Start.setBounds(235, 340, 130, 50);
-        
-        // Set màu chữ ban đầu
-        Start.setForeground(new Color(128,0,0));
-        // Làm cho nút trở nên trong suốt
-        Start.setOpaque(false);
-        Start.setContentAreaFilled(false);
-        Start.setBorderPainted(false);
-        
-        Start.setFocusable(false);
+       // Tạo ra một nút Start và đặt nội dung cho nó là Start
+       Start = new JButton("START");
+       Start.setFont(font);
+       Start.setBounds(220, 445, 150, 70);
+       
+       // Set màu chữ ban đầu
+       Start.setForeground(new Color(128,0,0));
+       // Làm cho nút trở nên trong suốt
+       Start.setOpaque(false);
+       Start.setContentAreaFilled(false);
+       Start.setBorderPainted(false);
+       
+       Start.setFocusable(false);
 
-        // Thêm trình nghe sự kiện chuột để thay đổi màu chữ khi di chuột vào
-        Start.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                Start.setForeground(new Color(94,8,8)); // Đổi màu chữ khi di chuột vào
-            }
+       Start.addMouseListener(new MouseAdapter() {
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            Start.setForeground(new Color(94,8,8)); // Đổi màu chữ khi di chuột vào
+        }
 
-            @Override
-            public void mouseExited(MouseEvent e) {
-                Start.setForeground(new Color(128,0,0)); // Đổi lại màu chữ khi chuột rời khỏi nút
-            }
-        });
-        
+        @Override
+        public void mouseExited(MouseEvent e) {
+            Start.setForeground(new Color(128,0,0)); // Đổi lại màu chữ khi chuột rời khỏi nút
+        }
+    });
         // Thêm nút Start vào introPanel với index = 0, là đặt nút này ở vị trí đầu tiên (trên cùng)
         introPanel.add(Start, 0);
+
+        // Tạo ra một nút scoreBoard và đặt nội dung cho nó là scoreBoard
+     scoreBoard = new JButton("SCORE BOARD");
+     scoreBoard.setFont(new Font("Arial", Font.BOLD, 19));
+     scoreBoard.setBounds(65, 555, 200, 50);
+     
+     // Set màu chữ ban đầu
+     scoreBoard.setForeground(new Color(128,0,0));
+     // Làm cho nút trở nên trong suốt
+     scoreBoard.setOpaque(false);
+     scoreBoard.setContentAreaFilled(false);
+     scoreBoard.setBorderPainted(false);
+     
+     scoreBoard.setFocusable(false);
+
+     scoreBoard.addMouseListener(new MouseAdapter() {
+      @Override
+      public void mouseEntered(MouseEvent e) {
+          scoreBoard.setForeground(new Color(94,8,8)); // Đổi màu chữ khi di chuột vào
+      }
+
+      @Override
+      public void mouseExited(MouseEvent e) {
+          scoreBoard.setForeground(new Color(128,0,0)); // Đổi lại màu chữ khi chuột rời khỏi nút
+      }
+  });
+        // Thêm nút Start vào introPanel với index = 0, là đặt nút này ở vị trí đầu tiên (trên cùng)
+        introPanel.add(scoreBoard, 1);
         
         // Tạo ra một nút Exit và đặt nội dung cho nó là Exit
-        Exit = new JButton("Exit");
-        Exit.setFont(font);
-        Exit.setBounds(235, 390, 130, 50);
+        Exit = new JButton("EXIT");
+        Exit.setFont(new Font("Arial", Font.BOLD, 20));
+        Exit.setBounds(380, 555, 130, 50);
         
         // Set màu chữ ban đầu
         Exit.setForeground(new Color(128,0,0));
@@ -97,6 +130,56 @@ public class Intro{
         });
 
         // Thêm nút Exit vào introPanel với index = 1, là đặt nút này ở vị trí sau nút Start (dưới cùng)
-        introPanel.add(Exit, 1);
+        introPanel.add(Exit, 2);
+     
+        // Tạo ra một nút loginButton nhấn sẽ đến cửa sổ đăng nhập
+        loginButton = new JButton("Đăng nhập");
+        loginButton.setFont(new Font("Arial", Font.BOLD, 16));
+        loginButton.setBounds(Mf.getJframeWidth()-135, Mf.getJframeHeight()-610, 130, 50);
+        
+        // Set màu chữ ban đầu
+        loginButton.setForeground(new Color(128,0,0));
+        // Làm cho nút trở nên trong suốt
+        loginButton.setOpaque(false);
+        loginButton.setContentAreaFilled(false);
+        loginButton.setBorderPainted(false);
+        
+        loginButton.setFocusable(false);
+        loginButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+               loginButton.setForeground(new Color(94,8,8)); // Đổi màu chữ khi di chuột vào
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                loginButton.setForeground(new Color(128,0,0)); // Đổi lại màu chữ khi chuột rời khỏi nút
+            }
+        });
+        introPanel.add(loginButton, 1);
+
     }
+
+
+
+    public JPanel getIntroPanel() {
+        return introPanel;
+    }
+
+    public JButton getStart() {
+        return Start;
+    }
+
+    public JButton getExit() {
+        return Exit;
+    }
+
+    public JButton getscoreBoard() {
+        return scoreBoard;
+    }
+
+    public JButton getLoginButton() {
+        return loginButton;
+    }
+    
 }
